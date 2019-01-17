17bb5f08176a337a22a9d117ae89795e3da6e3d7
SimonBasca <sbasca@mozilla.com>
2019-01-17 16:39:28 +0200

    New tests for Translation Feature

--- XCUITests/TranslationSnackBarTest.swift
+++ XCUITests/TranslationSnackBarTest.swift
@@ -5,15 +5,46 @@
 import XCTest
 
 class TranslationSnackBarTest: BaseTestCase {
-    func testSnackBar() {
+    
+    // // This test checks to the correct functionalty of the Translation prompt and Translation is done corrrectly using Google
+    func testSnackBarDisplayed() {
         userState.localeIsExpectedDifferent = true
         navigator.openURL(path(forTestPage: "manifesto-zh-CN.html"))
         waitUntilPageLoad()
         navigator.goto(TranslatePageMenu)
         waitForExistence(app.buttons["TranslationPrompt.doTranslate"])
+        app.buttons["TranslationPrompt.doTranslate"].tap()
+        waitForValueContains(app.textFields["url"], value: "translate.google")
+        if iPad(){
+                app.buttons["URLBarView.backButton"].tap()
+        } else{
+                app.buttons["TabToolbar.backButton"].tap()
+        }
+        app.buttons["TranslationPrompt.dontTranslate"].tap()
+        XCTAssertFalse(app.buttons["TranslationPrompt.dontTranslate"].exists)
     }
-
-    func testSetting() {
+    
+    // This test checks to see if Translation is enabled by default from the Settings menu and can be correctly disabled
+    func testSnackBarNotDispalyed() {
         navigator.goto(TranslationSettings)
+        let translationToggle = app.tables.cells.element(boundBy:0)
+        XCTAssertTrue(translationToggle.isEnabled)
+        translationToggle.tap()
+        navigator.openURL(path(forTestPage: "manifesto-zh-CN.html"))
+        waitUntilPageLoad()
+        XCTAssertFalse(app.buttons["TranslationPrompt.dontTranslate"].exists)
+    }
+    
+    // This test checks to see if Translation is correctly done when using Bing
+    func testTranslateBing() {
+        navigator.goto(TranslationSettings)
+        app.tables["Translate.Setting.Options"].staticTexts["Bing"].tap()
+        navigator.openURL(path(forTestPage: "manifesto-zh-CN.html"))
+        waitUntilPageLoad()
+        //navigator.goto(TranslatePageMenu)
+        waitForExistence(app.buttons["TranslationPrompt.doTranslate"])
+        app.buttons["TranslationPrompt.doTranslate"].tap()
+        waitForValueContains(app.textFields["url"], value: "microsofttranslator")
     }
 }
+
